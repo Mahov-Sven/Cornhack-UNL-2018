b7fadf899621715ae3382b6ed74ad6215c34dd91
Ethan Penn <empenn54@gmail.com>
2018-04-14 15:32:03 -0500

    Added global vars

--- src/cornhack/main/Globals.java
+++ src/cornhack/main/Globals.java
@@ -2,5 +2,11 @@ package cornhack.main;
 
 
 public class Globals {
-	
+	public static final int MINING_TIER_1 = 0;
+	public static final int MINING_TIER_2 = 1;
+	public static final int MINING_TIER_3 = 2;
+	public static final int MINING_TIER_4 = 3;
+
+	public static final int TERRAIN_DIFFICULTY_PASSABLE = 4;
+	public static final int TERRAIN_DIFFICULTY_IMPASSABLE = 5;
 }
