9a8c27dc83911cb8494c0acec93fc1d05c84e885
Ethan Penn <empenn54@gmail.com>
2018-04-14 13:55:21 -0500

    completed tile class

--- src/cornhack/world/tile/Tile.java
+++ src/cornhack/world/tile/Tile.java
@@ -2,6 +2,11 @@ package cornhack.world.tile;
 
 import java.util.ArrayList;
 
+import cornhack.creature.Creature;
+import cornhack.mechanics.actions.Action;
+import cornhack.world.Map;
+import javafx.scene.text.Text;
+
 public abstract class Tile {
 	
 	private int miningTier;
@@ -13,5 +18,11 @@ public abstract class Tile {
 	private ArrayList<Tile> notifyTiles;
 	
 	private boolean beenSeen;
+
+	public abstract void render(Text screen, Map map);
 	
+	public abstract boolean tryMove(Map map, Creature creature, Action action);
+	
+	@Override
+	public abstract boolean equals(Object obj);
 }
