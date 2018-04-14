ff0abe10976f36fe968ecd8c27027c212141d731
Maven <maven.r.losey@gmail.com>
2018-04-14 15:43:48 -0500

    andrew did this

--- src/cornhack/mechanics/actions/Defend.java
+++ src/cornhack/mechanics/actions/Defend.java
@@ -3,4 +3,11 @@ package cornhack.mechanics.actions;
 
 public class Defend {
 	
+	private boolean defend = false;
+	public boolean getDefend() {
+		return this.defend;
+	}
+	public void setDefend(boolean value) {
+		this.defend = value;
+	}
 }
