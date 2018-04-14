e52b10b24613956c353565b0d22eb23d28aade72
tboswell204 <tboswell204@gmail.com>
2018-04-14 16:03:50 -0500

    new creature/player methods

--- src/cornhack/creature/Player.java
+++ src/cornhack/creature/Player.java
@@ -1,6 +1,33 @@
 package cornhack.creature;
 
-
 public class Player {
+	
+	private String name;
+	//ChrisChristie, TerryCrews, DwayneJohnson, RayCharles
+	private String profession;
+	//Boxer, Spy, ChosenOne, Wizard
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProfession() {
+		return profession;
+	}
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+	public Player(String name, String profession) {
+		super();
+		this.name = name;
+		this.profession = profession;
+	}
+	public Player() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
 	
 }
