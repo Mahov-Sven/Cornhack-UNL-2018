0f9532be9614554e2e9e37bdba9f7f910cf0d164
Andrew Buckwalter <bckwalter44@gmail.com>
2018-04-14 14:39:46 -0500

    Adds required methods to Floor class

--- src/cornhack/world/tile/Floor.java
+++ src/cornhack/world/tile/Floor.java
@@ -1,6 +1,28 @@
 package cornhack.world.tile;
 
+import cornhack.creature.Creature;
+import cornhack.mechanics.actions.Action;
+import cornhack.world.Map;
+import javafx.scene.text.Text;
 
 public class Floor extends Tile{
+	
+	@Override
+	public void render(Text screen, Map map) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean tryMove(Map map, Creature creature, Action action) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 }
