b5c2c352a6d09cbca0d4cbbe48e04299ff633058
Ethan Penn <empenn54@gmail.com>
2018-04-14 15:18:16 -0500

    Started work on map

--- src/cornhack/util/Matrix.java
+++ src/cornhack/util/Matrix.java
@@ -1,6 +1,67 @@
 package cornhack.util;
 
+import java.util.ArrayList;
 
-public class Matrix {
+public class Matrix<T> {
 	
+	private ArrayList<ArrayList<T>> matrix;
+	
+	public Matrix() {
+		this.matrix = new ArrayList<ArrayList<T>>();
+	}
+	
+	public Matrix(int rows, int cols) {
+		this.matrix = new ArrayList<ArrayList<T>>();
+		this.size(rows, cols);
+	}
+
+	public void size(int rows, int cols) {
+		ArrayList<ArrayList<T>> result = new ArrayList<ArrayList<T>>();
+		for (int row = 0; row < rows; row++) {
+			if (row >= matrix.size())
+				break;
+
+			for (int col = 0; col < cols; col++) {
+				if (col >= matrix.get(col).size())
+					break;
+
+				result.get(row).set(col, matrix.get(row).get(col));
+			}
+		}
+	}
+
+	public T get(int row, int col) {
+		return matrix.get(row).get(col);
+	}
+	
+	public int[] find(T elem) {
+		for (int row = 0; row < matrix.size(); row++) {
+			for (int col = 0; col < matrix.get(row).size(); col++) {
+				T curr = matrix.get(row).get(col);
+				if (elem == curr || elem.equals(curr))
+					return new int[] { row, col };
+			}
+		}
+		return null;
+	}
+
+	public T remove(int row, int col) {
+		return this.set(row, col, null);
+	}
+
+	public T set(int row, int col, T elem) {
+		return matrix.get(row).set(col, elem);
+	}
+
+	public int width() {
+		return matrix.get(0).size();
+	}
+
+	public int height() {
+		return matrix.size();
+	}
+
+	public int length() {
+		return this.height() * this.width();
+	}
 }
--- src/cornhack/world/Map.java
+++ src/cornhack/world/Map.java
@@ -1,6 +1,43 @@
 package cornhack.world;
 
+import cornhack.creature.Creature;
+import cornhack.util.Matrix;
+import cornhack.world.tile.Tile;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class Map {
 	
+	private Matrix<Tile> map;
+	private Matrix<Creature> creatures;
+	
+	public Map(int height, int width) {
+		this.map = new Matrix<Tile>(height, width);
+		this.creatures = new Matrix<Creature>(height, width);
+	}
+
+	public TextFlow render() {
+		Text[] tiles = new Text[map.length()];
+		
+		for (int y = 0; y < map.height(); y++) {
+			for (int x = 0; x < map.width(); x++) {
+				int index = y * map.width() + x;
+				if (creatures.get(y, x) != null) {
+					tiles[index] = creatures.get(y, x).render();
+				} else if (map.get(y, x) != null) {
+					tiles[index] = map.get(y, x).render();
+				}
+			}
+		}
+		
+		return new TextFlow(tiles);
+	}
+
+	public Matrix<Tile> getMap() {
+		return map;
+	}
+
+	public Matrix<Creature> getCreatures() {
+		return creatures;
+	}
 }
